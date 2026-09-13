81c51d170525ea741149f07d586679dbf8b3a09e
Elaine Huang <71571680+huang12211@users.noreply.github.com>
2026-09-13 15:06:25 -0400

    updated Vistual.spec.tx

--- tests/e2e/Visual.spec.ts
+++ tests/e2e/Visual.spec.ts
@@ -7,24 +7,50 @@ test.describe('Visual testing', () => {
       await page.goto('/');
 
       await expect(
-        page.getByRole('heading', {
-          name: 'Boilerplate Code for Your Next.js Project with Tailwind CSS',
-        }),
+        page.getByText('Elaine Huang', { exact: false }).first(),
       ).toBeVisible();
 
       await percySnapshot(page, 'Homepage');
     });
 
-    test('should take screenshot of the about page', async ({ page }) => {
-      await page.goto('/about');
+    test('should take screenshot of the design work page', async ({ page }) => {
+      await page.goto('/design');
 
       await expect(
-        page.getByRole('link', {
-          name: 'About',
-        }),
+        page.getByRole('heading', { name: 'Design Work' }),
       ).toBeVisible();
 
-      await percySnapshot(page, 'About');
+      await percySnapshot(page, 'Design Work');
+    });
+
+    test('should take screenshot of the development work page', async ({ page }) => {
+      await page.goto('/development');
+
+      await expect(
+        page.getByRole('heading', { name: 'Development Work' }),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Development Work');
+    });
+
+    test('should take screenshot of the resume page', async ({ page }) => {
+      await page.goto('/resume');
+
+      await expect(
+        page.getByRole('heading', { name: 'Elaine Huang' }),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Resume');
+    });
+
+    test('should take screenshot of the contact page', async ({ page }) => {
+      await page.goto('/contact');
+
+      await expect(
+        page.getByRole('heading', { name: 'Let\'s connect!' }),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Contact');
     });
   });
 });
